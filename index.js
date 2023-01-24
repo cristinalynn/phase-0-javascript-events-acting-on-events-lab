a6b2e6d12f110b10f9ffa2db9a9a0006fe8a291e
Cristina Lynn <cristina@cristinalynn.net>
2023-01-23 19:25:37 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+function moveDodgerLeft() {
+    const leftNumber = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumber, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+ function moveDodgerRight() {
+    const leftNumber = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumber, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left + 1}px`;
+    }
+ }
